f3f3095e4e87a2c1963d1efb33fa815fff45c5d0
Shadi Zade <oldoakstem@gmail.com>
2023-04-19 23:38:58 +0300

    restructure ql script + detect mov

--- polybar/scripts/quodlibet-polybar.sh
+++ polybar/scripts/quodlibet-polybar.sh
@@ -14,21 +14,27 @@
 ###################################################################################################
 ####### symlink location: ~/.config/polybar/scripts/ ##############################################
 
-quodlibet --print-playing 2>&1 | grep -q 'not running' && exit
+quodlibet --print-playing 2>&1 | grep -qi 'not running' && exit
 fullname=$(quodlibet --print-playing)
 echo "$fullname" | grep -q 'Unknown Audio' && exit
 echo "$fullname" | grep -q '[ابتثجحخدذرزسشصضطظعغفقكلمنهوي]' && ~/.config/polybar/scripts/arab-quodlibet-polybar.sh && exit
-# checks for a hyphen to ascertain if the song has a known artist
-if printf "$fullname" | grep -q ' - '
-	then
-	songitself=$(printf "$fullname" | awk -F " - " '{print $2}' | awk -F '(' '{print $1}' | sed 's/\s*$//g')
-	artistname=$(printf "$fullname" | awk -F " - " '{print $1}')
+
+function isolate-fullname {
+	# checks for a hyphen to ascertain if the song has a known artist
+	songitself=$(printf "$1" | awk -F " - " '{print $2}' | awk -F '(' '{print $1}' | sed 's/\s*$//g')
+	artistname=$(printf "$1" | awk -F " - " '{print $1}')
 	# removes II, III, Jr, or Jr. from artist name, isolates surname, and finalizes structure
 	songname=$(printf "$artistname" | awk -F " (Jr[.]*|II[I]*)" '{print $1}' | awk -F ' ' '{print $NF}')"'s "$songitself
-	else
-	songname=$fullname
-fi
+}
+function get-mov {
+	movnum=""
+	echo "$1" | grep -Gq "—" || return
+	movnum=$(echo " [$(echo "$1" | awk -F " — " '{print $2}' | awk -F "." '{print $1}')]")
+}
+
+printf "$fullname" | grep -q ' - ' && isolate-fullname "$fullname" || songname="$fullname"
 # checks for all categorizations (Op., BWV, B., Hob.), and then for movement #s
-printf "$songname" \
-| awk -F ',[[:space:]][[:alnum:]]+[[:print:]]+[([:digit:]|N/A)]' '{print $1}' \
-| awk -F '-[[:space:]][IVX]+[[:space:]]' '{print $1}'
+songname=$(echo "$songname" \
+| awk -F ' —' '{print $1}' \
+| awk -F ', [[:alnum:]]+[[:print:]]+[([:digit:]|N/A)]' '{print $1}')
+get-mov "$fullname"; echo "$songname$movnum"
