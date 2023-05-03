9983a241c2170435b42da03d576f4ddf9a65b21d
Shadi Zade <oldoakstem@gmail.com>
2023-05-04 01:44:12 +0300

    rewrite emacs init

--- emacs/init.el
+++ emacs/init.el
@@ -1,91 +1,43 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
-;;;					 ____  _   _    _    ____ ___     _____   _    ____  _____ 
-;;;					/ ___|| | | |  / \  |  _ \_ _|   |__  /  / \  |  _ \| ____|
-;;;					\___ \| |_| | / _ \ | | | | |_____ / /  / _ \ | | | |  _|  
-;;;					 ___) |  _  |/ ___ \| |_| | |_____/ /_ / ___ \| |_| | |___ 
-;;;					|____/|_| |_/_/   \_\____/___|   /____/_/   \_\____/|_____|
-;;;
-;;;					https://github.com/ShadiZade
-;;;					init.el
-;;;					emacs config
-;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;; symlink location: ~/.config/emacs/ ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-;;; Basic appearance options
 (setq inhibit-startup-message t)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(menu-bar-mode -1)
-(set-fringe-mode 20)
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
-;;(setq ido-enable-flex-matching t)
-;;(setq ido-everywhere t)
-;;(ido-mode 1)
-(ivy-mode 1)
-(setq make-backup-files nil)
-(setq visible-bell 1)
 
-;; Initialize package sources
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(tooltip-mode -1)
+(set-fringe-mode 16)
+(menu-bar-mode -1)
+(setq visible-bell t)
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+(setq make-backup-files nil)
+(setq line-move-visual t)
+(global-visual-line-mode 1)
+(load-theme 'tango-dark)
+(ivy-mode 1)
+(xclip-mode 1)
+(set-face-attribute 'default nil :font "Fira Code Retina" :height 120)
+
+;; init package sources
 (require 'package)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("org" . "https://orgmode.org/elpa/")
-                         ("elpa" . "https://elpa.gnu.org/packages/")))
-
+			 ("org" . "https://orgmode.org/elpa")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(bongo-default-directory nil)
- '(custom-enabled-themes '(tango-dark))
- '(custom-safe-themes
-   '("a556e4e6fc62469cd28a57c3b5386807d676a33176659f849fc53fa8763f5955" default))
- '(elfeed-feeds
-   '("https://babelwad.com/feed/" "https://www.smbc-comics.com/comic/rss" "https://phdcomics.com/gradfeed.php" "https://xkcd.com/atom.xml" "https://simonwillison.net/atom/entries/" "https://pluralistic.net/feed/"))
- '(global-visual-line-mode t)
- '(mediawiki-site-alist nil)
  '(package-selected-packages
-   '(mediawiki lorem-ipsum lua-mode xclip graphviz-dot-mode markdown-mode slime yaml-mode elfeed powershell 2048-game counsel auto-package-update annotate)))
+   '(use-package yaml-mode xclip slime mediawiki markdown-mode lua-mode lorem-ipsum hydra graphviz-dot-mode dash counsel)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
-(setq inferior-lisp-program "sbcl")
-(require 'slime-autoloads)
-(eval-after-load "slime"  '(progn (slime-setup '(slime-fancy))))
-
-;;; Keybinds
-(global-set-key (kbd "M-o") 'other-window)
-(global-set-key (kbd "C-c f") 'flyspell-mode)
-(global-set-key (kbd "C-c l") #'org-store-link)
-(global-set-key (kbd "C-c a") #'org-agenda)
-(global-set-key (kbd "C-c c") #'org-capture)
-(global-set-key (kbd "C-c C-q") 'slime-close-all-parens-in-sexp)
-(global-set-key (kbd "C-c C-x C-/") 'strike-through-for-org-mode)
-(global-set-key (kbd "C-c C-<return>") 'org-insert-subheading)
-
-
-
-
-
-
-
-(load "beta-html-helper-mode")
-(setq auto-mode-alist (cons '("\\.html$" . html-helper-mode) auto-mode-alist))
-
-
-;; Fontsets setup
-
-(set-fontset-font "fontset-default" 'arabic "Kawkab Mono")
-(set-fontset-font "fontset-default" 'han "KN Maiyuan")
-(set-face-attribute 'default nil :font "Fira Code Retina" :height 120)
-
