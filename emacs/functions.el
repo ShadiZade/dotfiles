809ce2cff4be872d1ab4c662fd83e22913cdea3a
Shadi Zade <oldoakstem@gmail.com>
2023-12-02 17:08:05 +0800

    202312021708

--- emacs/functions.el
+++ emacs/functions.el
@@ -125,3 +125,11 @@
 
 (defalias 'do-xhtml-italic-region
   (kmacro "C-x C-x < i > C-x C-x < / i >"))
+
+(defalias 'yank-bib-and-copy-name
+   (kmacro "M-> C-y <return> C-s @ <return> M-f M-f C-SPC M-b M-w C-c C-c M-> C-x C-s"))
+
+;; maps
+
+(with-eval-after-load 'bibtex-mode
+  (define-key bibtex-mode-map (kbd "C-c C-~") 'yank-bib-and-copy-name))
