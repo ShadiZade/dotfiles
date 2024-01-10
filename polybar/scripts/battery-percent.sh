ce03b7d72b52fdbc80de5623e1d9ec457b6df805
Shadi Zade <oldoakstem@gmail.com>
2024-01-10 16:18:19 +0800

    202401101618

--- polybar/scripts/battery-percent.sh
+++ polybar/scripts/battery-percent.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-upower -i /org/freedesktop/UPower/devices/battery_BAT1 | grep percentage | awk '{print $2}'
+state="$(upower -i /org/freedesktop/UPower/devices/battery_BAT1 | grep state | awk '{print $2}')"
+case "$state" in
+    charging)
+	symbol=Ʇ ;;
+    discharging)
+	symbol=T ;;
+    *)
+	symbol=Ꝋ ;;
+esac
+echo $symbol $(upower -i /org/freedesktop/UPower/devices/battery_BAT1 | grep percentage | awk '{print $2}')
