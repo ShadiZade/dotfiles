096aa66696825cc32188d2fbbd8f04099b52cfc4
Shadi Zade <shadi@zookeeper.localdomain>
2024-10-14 09:22:38 +0800

    202410140922

--- emacs/init.el
+++ emacs/init.el
@@ -49,7 +49,7 @@
 ;; init package sources
 (require 'package)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-			 ("org" . "https://orgmode.org/elpa")
+			;; ("org" . "https://orgmode.org/elpa")
 			 ("elpa" . "https://elpa.gnu.org/packages/")))
 (package-initialize)
 (unless package-archive-contents
