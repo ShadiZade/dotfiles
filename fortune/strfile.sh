0b67da58cd1ee7f7f85be25d39793bbfde36db8c
ShadiZade <oldoakstem@gmail.com>
2025-01-15 21:18:34 +0800

    202501152118

--- fortune/strfile.sh
+++ fortune/strfile.sh
@@ -2,12 +2,11 @@
 source ~/Repositories/scripts/essential-functions
 
 strfile "$HOME"/Repositories/dotfiles/fortune/marxism "$HOME"/Repositories/dotfiles/fortune/marxism.dat 
-grep --color=auto -is —— marxism         \
-    | awk -F ',' '{print $1}'            \
-    | awk -F '(' '{print $1}'            \
-    | grep --color=auto -is -v ' and '   \
-    | sed 's/ *$//g;s/—— //g'            \
-    | sort                               \
+grep --color=auto -is —— marxism            \
+    | awk -F ',' '{print $1}'               \
+    | awk -F '(' '{print $1}'               \
+    | sed 's/ *$//g;s/—— //g;s/ and /\n/g'  \
+    | sort                                  \
 	  > marxism.stats
 echolor neonblue "——————————————————————————————————————————"
 echolor neonblue "$(cat marxism.stats | uniq -c | sed 's/^ *//g' | sort -V)"
