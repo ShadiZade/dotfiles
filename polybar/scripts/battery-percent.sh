134b694636fd8763a9dfe7a189c5a8962caee952
ShadiZade <oldoakstem@gmail.com>
2025-03-03 19:22:26 +0800

    202503031922

--- polybar/scripts/battery-percent.sh
+++ polybar/scripts/battery-percent.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
 adapter_state="$(acpi -a | awk '{print $NF}')"
+battery_results="$(acpi -b)"
+bat0_percent="$(sed -n 1p <<< "$battery_results" | awk -F ', ' '{print $2}' | sed 's/100%/100/')"
+bat1_percent="$(sed -n 2p <<< "$battery_results" | awk -F ', ' '{print $2}' | sed 's/100%/100/')"
 case "$adapter_state" in
     "on-line")
-	symbol=▲ ;;
+	symbol=▲ 
+	case "$(grep ": Charging" <<< "$battery_results" | awk '{print $2}' )" in
+	    "0:") echo "$symbol $bat0_percent • $bat1_percent •" ;;
+	    "1:") echo "• $bat0_percent • $bat1_percent $symbol" ;;
+	    *) echo "• $bat0_percent $symbol $bat1_percent •" ;;
+	esac
+	;;
     "off-line")
-	symbol=▼ ;;
+	symbol=▼
+	case "$(grep ": Discharging" <<< "$battery_results" | awk '{print $2}')" in
+	    "0:") echo "$symbol $bat0_percent • $bat1_percent •" ;;
+	    "1:") echo "• $bat0_percent • $bat1_percent $symbol" ;;
+	    *) echo "• $bat0_percent $symbol $bat1_percent •" ;;
+	esac
+	;;
 esac
-battery_results="$(acpi -b)"
-bat0_percent="$(sed -n 1p <<< "$battery_results" | awk -F ', ' '{print $2}')"
-bat1_percent="$(sed -n 2p <<< "$battery_results" | awk -F ', ' '{print $2}')"
-case "$(grep -v ": Not charging" <<< "$battery_results" | awk '{print $2}' )" in
-    "0:") echo "$symbol $bat0_percent • $bat1_percent •" ;;
-    "1:") echo "• $bat0_percent • $bat1_percent $symbol" ;;
-    *) echo "• $bat0_percent $symbol $bat1_percent •" ;;
-esac	  
+
+
 
