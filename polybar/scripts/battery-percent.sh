0ba1ee9158c2192038d5b1139b3b48a562841dd7
ShadiZade <oldoakstem@gmail.com>
2025-03-03 13:10:09 +0800

    202503031310

--- polybar/scripts/battery-percent.sh
+++ polybar/scripts/battery-percent.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-state="$(acpi -a | awk '{print $NF}')"
-case "$state" in
+adapter_state="$(acpi -a | awk '{print $NF}')"
+case "$adapter_state" in
     "on-line")
 	symbol=▲ ;;
     "off-line")
 	symbol=▼ ;;
 esac
-bat0_percent="$(acpi -b | sed -n 1p | awk -F ', ' '{print $2}')"
-bat1_percent="$(acpi -b | sed -n 2p | awk -F ', ' '{print $2}')"
-echo "$bat0_percent $symbol $bat1_percent"
+battery_results="$(acpi -b)"
+bat0_percent="$(sed -n 1p <<< "$battery_results" | awk -F ', ' '{print $2}')"
+bat1_percent="$(sed -n 2p <<< "$battery_results" | awk -F ', ' '{print $2}')"
+case "$(grep -v ": Not charging" <<< "$battery_results" | awk '{print $2}' )" in
+    "0:") echo "$symbol $bat0_percent • $bat1_percent •" ;;
+    "1:") echo "• $bat0_percent • $bat1_percent $symbol" ;;
+    *) echo "• $bat0_percent $symbol $bat1_percent •" ;;
+esac	  
+
