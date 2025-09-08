d5d1eb99e67e20d4eacec4e38cf031b947b44033
ShadiZade <oldoakstem@gmail.com>
2025-09-08 09:34:39 +0800

    202509080934

--- polybar/scripts/quodlibet-polybar.sh
+++ polybar/scripts/quodlibet-polybar.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
-##################################################################################################
-#
-#					 ____  _   _    _    ____ ___     _____   _    ____  _____ 
-#					/ ___|| | | |  / \  |  _ \_ _|   |__  /  / \  |  _ \| ____|
-#					\___ \| |_| | / _ \ | | | | |_____ / /  / _ \ | | | |  _|  
-#					 ___) |  _  |/ ___ \| |_| | |_____/ /_ / ___ \| |_| | |___ 
-#					|____/|_| |_/_/   \_\____/___|   /____/_/   \_\____/|_____|
-#
-#					https://github.com/ShadiZade
-#					quodlibet-polybar.sh
-#					script for displaying name of song in a polybar module
-#
-###################################################################################################
-####### symlink location: ~/.config/polybar/scripts/ ##############################################
+# location: ~/.config/polybar/scripts/
 
 [[ -e "/tmp/brown-noise-pid" ]] && {
     echo "[BROWN NOISE]"
     exit
 }
-quodlibet --print-playing 2>&1 | grep -qi 'not running' && exit
+quodlibet --print-playing 2>&1 | grep -qi 'not running' && {
+    echo ''
+    exit
+}
 fullname=$(quodlibet --print-playing)
-echo "$fullname" | grep -q 'Unknown Audio' && exit
-echo "$fullname" | grep -q '[ابتثجحخدذرزسشصضطظعغفقكلمنهوي]' && ~/.config/polybar/scripts/arab-quodlibet-polybar.sh && exit
+echo "$fullname" | grep -q 'Unknown Audio' && {
+    echo '[UNKNOWN AUDIO]'
+    exit
+}
+echo "$fullname" | grep -q '[ابتثجحخدذرزسشصضطظعغفقكلمنهوي]' && {
+    if echo "$fullname" | grep -q ' - '
+    then
+	songitself=$(echo "$fullname" | awk -F " - " '{print $2}')
+	artistname=$(echo "$fullname" | awk -F " - " '{print $1}')
+	songname=$songitself" ل"$artistname
+    else
+	songname="$fullname"
+    fi
+    echo "$songname" | fribidi | awk '{gsub(/^\s+|\s+$/,"")} {print $0}'
+    exit
+}
 
 function isolate-fullname {
 	# checks for a hyphen to ascertain if the song has a known artist
@@ -30,7 +34,7 @@ function isolate-fullname {
 	# removes II, III, Jr, or Jr. from artist name, isolates surname, and finalizes structure
 	songname=$(printf "$artistname" | awk -F " (Jr[.]*|II[I]*)" '{print $1}' | awk -F ' ' '{print $NF}')"'s "$songitself
 }
-function get-mov {
+function get-movement {
 	movnum=""
 	echo "$1" | grep -Gq "—" || return
 	movnum=$(echo " [$(echo "$1" | awk -F " — " '{print $2}' | awk -F "." '{print $1}')]")
@@ -41,4 +45,4 @@ printf "$fullname" | grep -q ' - ' && isolate-fullname "$fullname" || songname="
 songname=$(echo "$songname" \
 | awk -F ' —' '{print $1}' \
 | awk -F ', [[:alnum:]]+[[:print:]]+[([:digit:]|N/A)]' '{print $1}')
-get-mov "$fullname"; echo "$songname$movnum"
+get-movement "$fullname"; echo "$songname$movnum"
