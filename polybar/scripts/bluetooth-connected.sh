f3da1b3ccf0e96a3cf61dfb5fb59d3bae99176e2
ShadiZade <oldoakstem@gmail.com>
2026-03-18 20:03:21 +0800

    202603182003

--- polybar/scripts/bluetooth-connected.sh
+++ polybar/scripts/bluetooth-connected.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 echo info | bluetoothctl | grep -q 'Connected: yes' && echo -n '' || echo -n '⬯'
+case "$(pamixer --get-mute)" in
+    "true")
+	echo -n " MUT" ;;
+    "false")
+	echo -n " $(pamixer --get-volume)%" ;;
+esac
